4425dd7ee84fd4c4839aebc58deeafa853c5b490
aghastmuffin <78246775+aghastmuffin@users.noreply.github.com>
2024-04-07 10:11:49 -0700

    try to fix FileNotFoundError: [Errno 2] No such file or directory: '/home/runner/work/spotify-cli/spotify-cli/README.md' ERROR Backend subprocess exited when trying to invoke get_requires_for_build_sdist

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from os import path
 
 # read the contents of your README file
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+with open('README.md', encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
