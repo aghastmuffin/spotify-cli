841708e98929946bf2bf450332c0a494da447bee
aghastmuffin <78246775+aghastmuffin@users.noreply.github.com>
2024-04-07 00:03:57 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+# Fabricated long description
+long_description = "dynamic"
+# Delegate all other configuration to pyproject.toml
+
+setup(
+    name="spotify-win-cli",
+    version="3.0.0",
+)
