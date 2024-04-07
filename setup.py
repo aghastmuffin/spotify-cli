72667cf0b091de76d1077b2a85557bf79d5809fd
aghastmuffin <78246775+aghastmuffin@users.noreply.github.com>
2024-04-07 10:09:44 -0700

    add readme.md + support for it

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from setuptools import setup
+from os import path
 
-# Fabricated long description
-long_description = "dynamic"
-# Delegate all other configuration to pyproject.toml
+# read the contents of your README file
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name="spotify-win-cli",
     version="3.0.0",
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
