20ea5b699aac23ab5c17a99cec992eda47705c32
aghastmuffin <78246775+aghastmuffin@users.noreply.github.com>
2024-04-07 12:09:32 -0700

    rm version, deffined in pyproject

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from os import path
 
 setup(
     name="spotify-win-cli",
-    version="4.0.1",
     long_description="This is a detailed description of my package.",
     long_description_content_type='text/plain'
 )
