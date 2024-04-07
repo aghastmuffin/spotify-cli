17077de89b7d5d3a6fa33b62cf4d142906e48a17
aghastmuffin <78246775+aghastmuffin@users.noreply.github.com>
2024-04-07 10:14:29 -0700

    PLEASE

--- setup.py
+++ setup.py
@@ -1,14 +1,9 @@
 from setuptools import setup
 from os import path
 
-# read the contents of your README file
-this_directory = path.abspath(path.dirname(__file__))
-with open('README.md', encoding='utf-8') as f:
-    long_description = f.read()
-
 setup(
     name="spotify-win-cli",
     version="3.0.0",
-    long_description=long_description,
-    long_description_content_type='text/markdown'
+    long_description="This is a detailed description of my package.",
+    long_description_content_type='text/plain'
 )
